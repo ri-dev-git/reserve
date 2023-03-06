90b420c2bc348422164fc61d6ad40fca947835d6
ri-dev-git <rishi9850@outloo.com>
2023-03-06 11:38:18 +0530

    UPDATE crontimings

--- cryptos/ada.js
+++ cryptos/ada.js
@@ -7,7 +7,7 @@ const balanceCall=require("./utils/updateBalance.js")
 const priceCall=require("./utils/updatePrice.js")
 
 
-  const address="addr1z8snz7c4974vzdpxu65ruphl3zjdvtxw8strf2c2tmqnxz2j2c79gy9l76sdg0xwhd7r0c0kna0tycz4y5s6mlenh8pq0xmsha"
+  const address="addr1v83pr86wyfkmvhalljkrhlfnryestrny34t44gaurmvx4tshfnjv3"
   const options = {
       method: 'get',
       url: `https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}`,
